afcdf9d71d18d176f2c566db22ffb8ac0b48f25f
sai3689 <95272832+sai3689@users.noreply.github.com>
2021-12-03 18:34:53 +0530

    Create Services.js

---
+++ src/components/pages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import Trips from '../Trips';
+function Services() 
+{
+  return (
+    <>
+      <Trips />
+    </>
+  );
+}
+export default Services;
