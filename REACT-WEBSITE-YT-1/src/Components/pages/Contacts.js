5f2b4fac28807379f48ff04a0eb787e14bd7c41b
sai3689 <95272832+sai3689@users.noreply.github.com>
2021-12-04 10:07:17 +0530

    Create Contacts.js

---
+++ REACT-WEBSITE-YT-1/src/Components/pages/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+function Contacts() 
+{
+  return (
+    <>
+      <Footer />
+     </>
+  );
+}
+export default Contacts;
