dafeadb9d833f733224b441fd1a891c145cd93ce
sai3689 <95272832+sai3689@users.noreply.github.com>
2021-12-04 10:09:16 +0530

    Create SignUp2.js

---
+++ REACT-WEBSITE-YT-1/src/Components/pages/SignUp2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import Login from '../Login';
+function SignUp2() 
+{
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+export default SignUp2;
